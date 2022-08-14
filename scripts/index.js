3b12f5e3880ee56dcee5ae2fb9b84b99f1a230d3
Pedro Victor <pedro20002victor@gmail.com>
2022-08-13 22:23:35 -0300

    ultimate update

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,3 @@
-let header = document.getElementById('header');
-let navigationHeader = document.getElementById('navbar');
-let mainContent = document.getElementById('main-content');
-let showSidebar = false;
-
-function toggleSidebar() {
-   showSidebar = !showSidebar;
-   if (showSidebar) {
-      navigationHeader.style.marginLeft = '-10vw'
-      navigationHeader.style.animationName = 'showSidebar'
-      mainContent.style.filter = 'blur(2px)'
-      mainContent.style.overflow = 'hidden'
-   }
-   else {
-      navigationHeader.style.marginLeft = '-100vw'
-      navigationHeader.style.animationName = ''
-      mainContent.style.filter = ''
-   }
-}
-
-function closeSidebar() {
-   if (showSidebar) {
-      toggleSidebar();
-   }
-}
-
-
-window.addEventListener('resize', () => {
-   if (window.innerWidth > 720 && showSidebar) {
-      toggleSidebar();
-   }
-});
-
 // API GITHUB - 
 let my_photo = document.querySelector('#my_photo');
 async function createAvatar() {
---
+++ scripts/menu_script.js
@@ -0,0 +1,32 @@
+let header = document.getElementById('header');
+let navigationHeader = document.getElementById('navbar');
+let mainContent = document.getElementById('main-content');
+let showSidebar = false;
+
+function toggleSidebar() {
+   showSidebar = !showSidebar;
+   if (showSidebar) {
+      navigationHeader.style.marginLeft = '-10vw'
+      navigationHeader.style.animationName = 'showSidebar'
+      mainContent.style.filter = 'blur(2px)'
+      mainContent.style.overflow = 'hidden'
+   }
+   else {
+      navigationHeader.style.marginLeft = '-100vw'
+      navigationHeader.style.animationName = ''
+      mainContent.style.filter = ''
+   }
+}
+
+function closeSidebar() {
+   if (showSidebar) {
+      toggleSidebar();
+   }
+}
+
+
+window.addEventListener('resize', () => {
+   if (window.innerWidth > 720 && showSidebar) {
+      toggleSidebar();
+   }
+});
