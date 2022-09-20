942b6f952fb832b45a44c96e659fb82ec4583d4f
Pedro Victor <pedro20002victor@gmail.com>
2022-09-19 23:22:09 -0300

    Color mode button

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,14 @@
 // API GITHUB - 
 let my_photo = document.querySelector('#my_photo');
 async function createAvatar() {
-   const userData = await fetch('https://api.github.com/users/PedroBVictor');
+   const userData = await fetch('https://api.github.com/users/PedroVictor022');
    const resp = await userData.json();
 
    my_photo.innerHTML = `<img class='my_photo' src="${resp.avatar_url}"/>` 
 }
 
 createAvatar();
+
+let btnDark = document.querySelector('#btnColorMode');
+
+btnDark.innerHTML = '<p>Mode</p>'
